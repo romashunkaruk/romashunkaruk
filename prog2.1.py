d6f22f542d47cdb73244145c98288b07aee7bea2
romashunkaruk <32163668+romashunkaruk@users.noreply.github.com>
2017-09-21 15:26:43 +0300

    Add files via upload

---
+++ prog2.1.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python3
+
+import sys
+
+name, surname, fathername = input("Введіть ПІБ: ").split(sep ='') 
+print("Hello Roman Vasilovuch , " + name + " " + surname + " " + fathername + " " )|
