78f34662678c8a9f00ae66744324e3a2c8beb363
romashunkaruk <32163668+romashunkaruk@users.noreply.github.com>
2017-09-28 11:26:01 +0300

    Add files via upload

---
+++ prog5.1.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+
+import sys 
+
+number = input("Введіть додатнє ціле число : ")
+conversetoInt = int(number)
+temp = (conversetoInt & (conversetoInt - 1)) == 0
+if temp == True :
+        print ("число є степенем двійки")
+elif temp ==False :
+        print ("число не є степенем двійки")
+
+
+
+
